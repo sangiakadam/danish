290c1d8ce067314d06e361bd5fbb0b61c1f14620
sangiakadam <sangitakadam118@proton.me>
2024-10-27 09:23:57 +0000

    compalet project

--- src/main.js
+++ src/main.js
@@ -2,24 +2,35 @@ console.log("connected");
 
 
 //List all breeds == https://dog.ceo/api/breeds/list/all
-// Breeds list == https://dog.ceo/api/breed/affenpinscher/images/random
+// Breeds list == https://dog.ceo/api/breed/{}/images/random
 
 //dom  selection 
 
-const selectE1=document.querySelector("#dog-breeds");
-console.log(selectE1);
+const selectEl=document.querySelector("#dog-breeds");
+const imsgeEl=document.querySelector("#dog=image");
+console.log(selectEl);
+console.log(imsgeEl);
 
 
 
 
 function getDogsBreed(){
 
-     return fetch("https://dog.ceo/api/breeds/list/all")
+     return fetch(`https://dog.ceo/api/breeds/list/all`)
     .then((responce)=>responce.json())
     .then((data)=>Object.keys(data.message))
     .catch((error)=>console.log(error));
 
     }
+
+function getDogImage(breed){
+    return fetch(`https://dog.ceo/api/breed/${breed}/images/random`)
+    .then((responce)=>responce.json())
+    .then((data)=>data.message)
+    .catch((error)=>console.log(error));
+    
+    
+}
 
 
 function renderOption(){
@@ -27,11 +38,15 @@ function renderOption(){
         for (let breed of breeds){
         const option=document.createElement("option");
         option.textContent=breed[0].toUpperCase()+breed.slice(1);
-
-        option.value=breed;s
+        option.value=breed;
         selectE1.appendChild(option);  
                   
         }
     })
 }
 renderOption()
+
+selectE1.addEventListener("change",()=>{
+    console.log(e.target.value);
+    getDogImage(selectedValue).then((link)=> (imsgeE1.src=link));
+});s
