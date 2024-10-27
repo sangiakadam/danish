d299922e798d21ad8dbef5597304994876869919
sangiakadam <sangitakadam118@proton.me>
2024-10-27 08:51:48 +0000

    render dog brred with capitalizaes

--- src/main.js
+++ src/main.js
@@ -4,6 +4,14 @@ console.log("connected");
 //List all breeds == https://dog.ceo/api/breeds/list/all
 // Breeds list == https://dog.ceo/api/breed/affenpinscher/images/random
 
+//dom  selection 
+
+const selectE1=document.querySelector("#dog-breeds");
+console.log(selectE1);
+
+
+
+
 function getDogsBreed(){
 
      return fetch("https://dog.ceo/api/breeds/list/all")
@@ -17,8 +25,12 @@ function getDogsBreed(){
 function renderOption(){
     getDogsBreed().then((breeds)=>{
         for (let breed of breeds){
-            console.log(breed);
-            
+        const option=document.createElement("option");
+        option.textContent=breed[0].toUpperCase()+breed.slice(1);
+
+        option.value=breed;s
+        selectE1.appendChild(option);  
+                  
         }
     })
 }
